242ad2844abb41329fbfbe58ec891bab04caa09a
Uisma Lopes <uisma.lopes@ploomes.com>
2022-10-26 17:09:19 -0300

    update

--- src/commands/createComponent.ts
+++ src/commands/createComponent.ts
@@ -1,4 +1,4 @@
-import { Uri, window, workspace } from "vscode";
+import { Uri, window } from "vscode";
 import * as _path from "path";
 import * as fs from "fs";
 import { config } from "../config";
